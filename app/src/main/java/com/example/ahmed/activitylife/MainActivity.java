caba3bfbb00c03b6d363e392f089b7f4dbce6022
SyedAhmed96 <syedahmedullah96pk@gmail.com>
2018-09-12 11:28:12 +0500

    Initial commit

---
+++ app/src/main/java/com/example/ahmed/activitylife/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.ahmed.activitylife;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        notify("onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        notify("onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        notify("onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        notify("onDestroy");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        notify("onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        notify("onSaveInstanceState");
+    }
+
+    private void notify(String methodName) {
+        String name = this.getClass().getName();
+        String[] strings = name.split("\\.");
+        Toast.makeText(getApplicationContext(),
+                methodName + "" + strings[strings.length - 1],
+                Toast.LENGTH_LONG).show();
+    }
+
+}
+
+
